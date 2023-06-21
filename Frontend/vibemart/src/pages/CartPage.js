1dc112e2fe6eebde959393d3b2c8cdeb934ef6a3
pavitra <upavitrasai161229@gmail.com>
2023-06-21 10:40:36 +0530

    changes

--- Frontend/vibemart/src/pages/CartPage.js
+++ Frontend/vibemart/src/pages/CartPage.js
@@ -24,7 +24,7 @@ const CartPage = () => {
       cart?.map((item) => {
         total = total + item.price;
       });
-      return total.toLocaleString("en-US", {
+      return total.toLocaleString("rupees", {
         style: "currency",
         currency: "INR",
       });
